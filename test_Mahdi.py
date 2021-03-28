ea59faff6dcb1a2de1b211a67c5b7412b32ae435
MAHDI <mahdi.maghrebi@nih.gov>
2021-03-28 17:17:48 -0400

    tmp unit testing code uploaded

---
+++ test_Mahdi.py
@@ -0,0 +1,107 @@
+import sys
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+import numpy as np
+import wndcharm
+from wndcharm.FeatureVector import FeatureVector, GenerateFeatureComputationPlan, \
+        IncompleteFeatureSetError
+from wndcharm.utils import compare
+
+from os.path import dirname, sep, realpath, join, abspath, splitext, basename
+
+sys.path.insert(1, '/home/maghrebim2/Work/WND-CHARM/ROI/PR2_Review/wnd-charm/wndcharm')
+'''
+import os.path
+fullpath='/home/maghrebim2/Work/WND-CHARM/ROI/PR2_Review/wnd-charm/wndcharm/PyImageMatrix.py'
+filename='PyImageMatrix.py'
+path, filename = os.path.split(fullpath)
+filename, ext = os.path.splitext(filename)
+sys.path.append(path)
+module = __import__(filename)
+reload(module) # Might be out of date
+del sys.path[-1]
+#return module
+'''
+
+
+pychrm_test_dir = dirname( realpath( __file__ ) )
+wndchrm_test_dir = join( dirname( pychrm_test_dir ), 'wndchrm_tests' )
+test_dir = wndchrm_test_dir
+
+img_filename = "Intensity.tif"
+orig_img_filepath = pychrm_test_dir + sep + img_filename
+
+#input_image_path = sourcedir + sep + img_filename
+class TestFeatureCalculation( unittest.TestCase ):
+
+    def test_ProfileLargeFeatureSet( self ):
+        kwargs = {}
+        kwargs[ 'name' ] = img_filename
+        kwargs[ 'source_filepath' ] = orig_img_filepath
+        #kwargs[ 'feature_names' ] = fw.feature_names
+        #kwargs[ 'feature_computation_plan' ] = comp_plan
+        kwargs[ 'long' ] = True
+        kwargs[ 'x' ] = 0
+        kwargs[ 'y' ] = 0
+
+        ROI_width = 231
+        ROI_height = 20
+        kwargs[ 'w' ] = ROI_width
+        kwargs[ 'h' ] = ROI_height
+
+        kwargs[ 'sample_group_id' ] = 0
+       # top_left_tile_feats = FeatureVector( **kwargs ).GenerateFeatures( quiet=False, write_to_disk=False)
+
+        Sig_reference_filename = "Reference.sig"
+        Sig_reference_filepath = pychrm_test_dir + sep + Sig_reference_filename
+        with open( Sig_reference_filepath ) as infile:
+            #firstline = infile.readline()
+            #m = re.match( r'^(\S+)\s*(\S+)?$', firstline           
+            #class_id, input_fs_version = m.group( 1, 2 )
+            #input_fs_major_ver, input_fs_minor_ver = input_fs_version.split('.')                
+            #orig_source_tiff_path = infile.readline()
+          
+          #  vec_len = 5
+          # values = np.zeros( vec_len )
+           # names = [None] * vec_len
+            for i, line in enumerate( infile ):
+                if i is 0: 
+                    name = line.rstrip("\n").split(",")
+                if i is 1:
+                    values = line.rstrip("\n").split(",")
+              #  values[i] = float( val )
+
+        comp_plan = wndcharm.StdFeatureComputationPlans.getFeatureSet(True)
+
+        from PyImageMatrix import PyImageMatrix
+        original_px_plane = PyImageMatrix()
+        reference_sigs_Intensity = '/home/maghrebim2/Work/WND-CHARM/ROI/PR2_Review/wnd-charm/tests/pywndcharm_tests/Intensity.tif'
+        reference_sigs_Label = '/home/maghrebim2/Work/WND-CHARM/ROI/PR2_Review/wnd-charm/tests/pywndcharm_tests/Label.tif'
+
+        from PIL import Image
+        im = Image.open(reference_sigs_Label) 
+        imarray = np.array(im) 
+        height=imarray.shape[0]
+        width=imarray.shape[1]
+        flattenimarray=imarray.flatten()
+        classID=255
+        labeledMatrix = wndcharm.doubleArray(width*height)
+        i=0
+        for element in np.nditer(flattenimarray): 
+            labeledMatrix[i] = float(element)
+            i=i+1
+            #for i in range(16):
+             #   labledMatrix[i]=i
+
+        retval = original_px_plane.OpenImage2(reference_sigs_Intensity,width, height, labeledMatrix ,classID)
+
+        if 1 != retval:
+            errmsg = 'Could not build a wndchrm.PyImageMatrix from {0}, check the path.'
+            raise ValueError( errmsg.format( self.source_filepath ) )     
+
+
+        
+if __name__ == '__main__':
+    unittest.main()
